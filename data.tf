94c6d1b3486a5502c57d39200612e01e6b9ed69b
Joshua Tonga <git@joshuatonga.com>
2024-03-14 12:18:27 +0800

    Add data aws_subnets to support elb_subnets_mapping

--- data.tf
+++ data.tf
@@ -10,7 +10,7 @@ data "aws_ami" "amazon-linux-2" {
 }
 
 data "aws_subnet" "subnets" {
-  count = length(var.elb_subnets)
-  id    = var.elb_subnets[count.index]
-}
+  count = var.elb_subnets != [] ? length(var.elb_subnets) : length(var.elb_subnets_mapping)
 
+  id = var.elb_subnets != [] ? var.elb_subnets[count.index] : var.elb_subnets_mapping[count.index].subnet_id
+}
