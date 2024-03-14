1fb932da44796eff52335fb2a8e0d8e558f5657c
Joshua Tonga <git@joshuatonga.com>
2024-03-14 15:00:32 +0800

    Fix conditional check

--- main.tf
+++ main.tf
@@ -163,10 +163,10 @@ resource "aws_lb" "bastion_lb" {
   internal = var.is_lb_private
   name     = "${local.name_prefix}-lb"
 
-  subnets = var.elb_subnets != null ? var.elb_subnets : null
+  subnets = length(var.elb_subnets) != 0 ? var.elb_subnets : null
 
   dynamic "subnet_mapping" {
-    for_each = var.is_lb_private && var.elb_subnets == null ? var.elb_subnets_mapping : []
+    for_each = var.is_lb_private && length(var.elb_subnets) == 0 ? var.elb_subnets_mapping : []
     content {
       subnet_id            = subnet_mapping.value.subnet_id
       private_ipv4_address = subnet_mapping.value.private_ipv4_address
